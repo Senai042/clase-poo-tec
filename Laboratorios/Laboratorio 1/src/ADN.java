033a81a6b51bd39748b54836e8c3e80c2ddd59ad
Senai042 <127223592+Senai042@users.noreply.github.com>
2023-06-01 16:08:49 -0600

    Delete Laboratorios directory

--- Laboratorios/Laboratorio 1/src/ADN.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-public class ADN {
-    List<String> lista= new ArrayList<String>();
-    String []Muestras= {"A-T", "G-C", "C-G", "T,A"};
-    List<String>adn= Arrays.asList(Muestras);
-
-}
